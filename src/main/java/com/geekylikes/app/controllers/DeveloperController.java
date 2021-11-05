d13ed67a261048ba02e30c4ed826d9585d5ef5fb
Cliff Choiniere <crcnum4@gmail.com>
2021-11-04 21:08:25 -0400

    CRUD on Developers done

--- src/main/java/com/geekylikes/app/controllers/DeveloperController.java
+++ src/main/java/com/geekylikes/app/controllers/DeveloperController.java
@@ -3,7 +3,9 @@ package com.geekylikes.app.controllers;
 import com.geekylikes.app.models.Developer;
 import com.geekylikes.app.repositories.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -22,14 +24,41 @@ public class DeveloperController {
         return repository.findAll();
     }
 
-    @PostMapping
-    public @ResponseBody Developer createDeveloper(@RequestBody Developer newDeveloper) {
-        return repository.save(newDeveloper);
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Developer>> getDevelopersByCohort(@PathVariable Integer cohort) {
+        return new ResponseEntity<>(repository.findAllByCohort(cohort, Sort.by("name")), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public @ResponseBody Developer getOneDeveloper(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper) {
+        return new ResponseEntity<>(repository.save(newDeveloper), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public @ResponseBody Developer updateDeveloper(@PathVariable Long id, @RequestBody Developer updates) {
+        Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+//        updates.setId(developer.getId());
+//        return repository.save(updates);
+        if (updates.getName() != null) developer.setName(updates.getName());
+        if (updates.getEmail() != null) developer.setEmail(updates.getEmail());
+        if (updates.getCohort() != null) developer.setCohort(updates.getCohort());
+        if (updates.getLanguages() != null) developer.setLanguages(updates.getLanguages());
+
+        return repository.save(developer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyDeveloper(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
+++ src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
@@ -1,7 +1,16 @@
 package com.geekylikes.app.repositories;
 
 import com.geekylikes.app.models.Developer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+    List<Developer> findAllByCohort(Integer cohort, Sort sort);
+
+//    @Query("SELECT * FROM developer WHERE cohort = ?1 AND ?2 in languages")
+//    Developer findByCohortAndLanguage(Integer cohort, String language);
 }
