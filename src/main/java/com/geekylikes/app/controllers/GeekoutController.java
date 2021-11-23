413d7db6fd3be8f541bf3f3a3f605a476c06a5b9
Cliff Choiniere <crcnum4@gmail.com>
2021-11-22 21:22:09 -0500

    Likes/Approval system

--- src/main/java/com/geekylikes/app/controllers/GeekoutController.java
+++ src/main/java/com/geekylikes/app/controllers/GeekoutController.java
@@ -1,6 +1,9 @@
 package com.geekylikes.app.controllers;
 
+import com.geekylikes.app.models.approve.Approve;
+import com.geekylikes.app.models.developer.Developer;
 import com.geekylikes.app.models.geekout.Geekout;
+import com.geekylikes.app.repositories.ApproveRepository;
 import com.geekylikes.app.repositories.GeekoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -16,6 +20,9 @@ import java.util.List;
 public class GeekoutController {
     @Autowired
     private GeekoutRepository repository;
+
+    @Autowired
+    private ApproveRepository approveRepository;
 
     @GetMapping
     public ResponseEntity<Iterable<Geekout>> getAll() {
@@ -32,6 +39,19 @@ public class GeekoutController {
         System.out.println(geekout.getDeveloper().getId());
 
         return new ResponseEntity<>(repository.save(geekout), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/like/{id}")
+    public ResponseEntity<Geekout> likeById(@PathVariable Long id, @RequestBody Developer developer) {
+        Optional<Geekout> geekout = repository.findById(id);
+
+        if (geekout.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        Approve newApproval = new Approve(developer, geekout.get());
+        approveRepository.save(newApproval);
+        return new ResponseEntity<>(geekout.get(), HttpStatus.CREATED);
     }
 
 //    @PutMapping("/{id}")
--- src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
+++ src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
@@ -1,6 +1,7 @@
 package com.geekylikes.app.repositories;
 
 import com.geekylikes.app.models.developer.Developer;
+import com.geekylikes.app.models.language.Language;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +11,10 @@ public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
     List<Developer> findAllByCohort(Integer cohort, Sort sort);
     List<Developer> findAllByLanguages_id(Long id);
+
+    //get a list of developers that liked geekout
+    List<Developer> findAllByApprovals_geekout_id(Long id);
+
 //    @Query("SELECT * FROM developer WHERE cohort = ?1 AND ?2 in languages")
 //    Developer findByCohortAndLanguage(Integer cohort, String language);
 }
